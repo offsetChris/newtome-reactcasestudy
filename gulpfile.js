46a923684bdd88ebb61376de803d88b186f90d10
offsetchris <christina@offsetchris.com>
2017-10-06 21:52:16 -0500

    git ignore added and gulp watch and sass compile writen

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,32 @@ var watch = require('gulp-watch');
 // require gulp-sass for sass compile
 var sass = require('gulp-sass');
 
+// set sass input and output directories
+var input = 'src/sass/**/*.scss';
+var output = 'src/css';
 
-gulp.task('default', function(){
+// sass options 
+var sassOptions = {
+	errLogtoConsole:true,
+	outputStyle: 'expanded'
+}
 
+// sass compiler
+gulp.task('sass', function(){
+	return gulp.src(input)
+	.pipe(sass(sassOptions).on('error', sass.logError))
+	.pipe(replace('{timestamp}', (new Date()).toString().substr(4,17)))
+	.pipe(gulp.dest(output));
+});
+
+// sass watch. any change in .scss file will trigger compile
+gulp.task('sass-watch', function(){
+	return gulp.watch(input, ['sass'])
+	.on('change', function(event){
+		console.log('file ' + event.path + ' was ' + event.type + ', running tasks...');
+	});
+});
+
+gulp.task('default', ['sass', 'sass-watch'], function(){
+	console.log('Sass compiled and watch script activated!');
 });

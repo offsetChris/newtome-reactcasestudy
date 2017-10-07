e24034c1f4c4d43aa402b1b2190c49bf268c7df2
offsetchris <christina@offsetchris.com>
2017-10-06 21:30:16 -0500

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+// require gulp
+var gulp = require('gulp');
+// require gulp-watch for watch script
+var watch = require('gulp-watch');
+// require gulp-sass for sass compile
+var sass = require('gulp-sass');
+
+
+gulp.task('default', function(){
+
+});
